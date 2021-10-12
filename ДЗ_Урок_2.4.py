5ba0021a545305f54866324f943a00d865cedba5
Tatiana Hoza <tatiana-15@tut.by>
2021-10-12 20:56:49 +0300

    Упражнение 5

--- ДЗ_Урок_2.4.py
+++ ДЗ_Урок_2.4.py
@@ -4,14 +4,3 @@ for n, i in enumerate(a):
         print(n, i)
     else:
         print(n, i[:10])
-
-a = [7, 5, 3, 3, 2]
-b = float(input("Введите новый элемент рейтинга:"))
-i = 0
-for n in a:
-    if b <= n:
-        i = i + 1
-    else:
-        break
-a.insert(i, b)
-print(a)
