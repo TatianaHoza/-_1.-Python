dd97672acd72cca2a5e09c11637522fb38584142
Tatiana Hoza <tatiana-15@tut.by>
2021-10-19 18:47:39 +0300

    Упражнение 7

---
+++ ДЗ_Урок_4.7.py
@@ -0,0 +1,13 @@
+from itertools import count
+from math import factorial
+def fact():
+    for el in count(1):
+        yield factorial(el)
+gen = fact()
+i = 0
+for n in gen:
+    if i < 15:
+        print(n)
+        i += 1
+    else:
+        break
